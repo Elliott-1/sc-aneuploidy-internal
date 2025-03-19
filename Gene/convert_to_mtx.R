0efb143682a67db0152dbb7d569b0e61954334b6
Elliott-1 <145612116+Elliott-1@users.noreply.github.com>
2025-03-18 22:28:20 -0700

    Delete Gene directory

--- Gene/convert_to_mtx.R
+++
@@ -1,5 +0,0 @@
-library(Matrix)
-
-full_mtx <- readRDS("/net/noble/vol1/home/es1/aneuploidy/data/full_mtx.RDS")
-# Write the matrix to a Matrix Market file
-writeMM(full_mtx, file = "/net/noble/vol1/home/es1/aneuploidy/data/full_mtx.mtx")
